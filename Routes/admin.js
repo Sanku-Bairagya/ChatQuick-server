ef5a1ad6c0b28e558f226cc939233b6d8bd5e47f
Sanku-Bairagya <sankubairagya911@gmail.com>
2024-12-20 22:16:13 +0530

    appJS file changed

--- Routes/admin.js
+++ Routes/admin.js
@@ -6,12 +6,12 @@ import { adminOnly } from "../middlewares/auth.js";
 const app = express.Router();
 
 
-app.post("verify",adminLoginValidator(),validateHandler,adminLogin)
-app.get("logout",adminLogOut)
-app.get("stats",getDashboardStats)
-app.get("users",allUsers)
-app.get("chats",allChats)
-app.get("messages",allMessages)
+app.post("/verify",adminLoginValidator(),validateHandler,adminLogin)
+app.get("/logout",adminLogOut)
+app.get("/stats",getDashboardStats)
+app.get("/users",allUsers)
+app.get("/chats",allChats)
+app.get("/messages",allMessages)
 app.use(adminOnly);
 
 app.get("/",verifyAdmin)
